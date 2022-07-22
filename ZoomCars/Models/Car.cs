5598e28efd4a0a22d1cfbebbf45e54325519c6eb
Balamahendira <balamahendhira@gmail.com>
2022-07-22 10:50:46 +0530

    first commit

---
+++ ZoomCars/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace ZoomCars.Models
+{
+    public partial class Car
+    {
+        public Car()
+        {
+            Rentals = new HashSet<Rental>();
+        }
+
+        public int Vin { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int? CarSeats { get; set; }
+        public string CarType { get; set; }
+        public string CarAvailable { get; set; }
+
+        [Display(Name = "Total")]
+        public int? CarPrice { get; set; }
+
+        public virtual ICollection<Rental> Rentals { get; set; }
+    }
+}
